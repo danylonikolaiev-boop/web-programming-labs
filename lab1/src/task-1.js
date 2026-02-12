3820d6a925d035a4e9b821224c5e2919eb50ec60
Ніколаєв <danylo.nikolaiev@kgemt.org.ua>
2026-02-13 01:50:07 +0200

    Lab 1: Task 1 - Destructuring and spread/rest

---
+++ lab1/src/task-1.js
@@ -0,0 +1,59 @@
+function getFullName(user) {
+  const { firstName, lastName, middleName = "" } = user;
+
+  const initial1 = firstName[0] + "."; 
+  
+  if (middleName === "") {
+    const initial2 = middleName[0] + ".";
+    return `${lastName} ${initial1} ${initial2}`;
+  } else {
+    return `${lastName} ${initial1}`;
+  }
+}
+
+function mergeObjects(...objects){
+  const result = {};
+  for (let obj of objects) {
+    Object.assign(result, obj); 
+  }
+  return result;
+}
+
+function removeDuplicates(...arrays) {
+  let combinedArray = [];
+  for (let arr of arrays) {
+    combinedArray = [...combinedArray, ...arr];
+  }
+
+  const uniqueItems = new Set(combinedArray);
+
+  return [...uniqueItems];
+
+}
+
+function createUpdatedUser(user, update) {
+  const updated = {...user, ...update, };
+  if (update.address) {
+    updated.address = { ...user.address, ...update.address};
+  }
+
+  return updated;
+}
+
+const user = { name: "John", age: 25, address: { city: "Kyiv", zip: "01001" } };
+const updated = createUpdatedUser(user, { age: 26, address: { zip: "02002" } });
+
+console.log("=== Завдання 1: Деструктуризація та Spread/Rest ===");
+console.log(
+  "1.1:",
+  getFullName({
+    firstName: "Петро",
+    lastName: "Іванов",
+    middleName: "Сергійович",
+  }),
+);
+
+
+console.log("1.2:", mergeObjects({ a: 1 }, { b: 2 }, { a: 3, c: 4 }))
+console.log("1.3:",removeDuplicates([1, 2, 3], [2, 3, 4], [4, 5]))
+console.log("1.4:", updated)
