78a68cd9be628b89bba10159013a9d2a070e5774
Ніколаєв <danylo.nikolaiev@kgemt.org.ua>
2026-03-16 13:48:29 +0200

    Lab 4: Add Layout component and routing configuration

--- lab4/src/App.tsx
+++ lab4/src/App.tsx
@@ -1,11 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { BrowserRouter, Routes, Route, Navigate } from "react-router";
+import Layout from "./components/Layout/Layout";
+import TasksPage from "./pages/TasksPage/TasksPage";
+import TaskDetailPage from "./pages/TaskDetailPage/TaskDetailPage";
+import NewTaskPage from "./pages/NewTaskPage/NewTaskPage";
+import { useState } from "react";
+import { INITIAL_TASKS } from "./data/initialTasks";
+import type { Task } from "./types/task";
 
-function App() {
+export default function App() {
+  const [tasks, setTasks] = useState<Task[]>(INITIAL_TASKS);
 
+  const addTask = (newTask: Task) => {
+    setTasks((prevTasks) => [...prevTasks, newTask]);
+  };
+
+  const deleteTask = (taskId: string) => {
+    setTasks((prevTasks) => prevTasks.filter((task) => task.id !== taskId));
+  };
+
+  const updateTask = (updatedTask: Task) => {
+    setTasks((prevTasks) =>
+      prevTasks.map((task) => (task.id === updatedTask.id ? updatedTask : task))
+    );
+  };
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Navigate to="/tasks" replace />} />
+          <Route 
+            path="tasks" 
+            element={<TasksPage tasks={tasks} onDelete={deleteTask} />} 
+          />
+          <Route 
+            path="tasks/new" 
+            element={<NewTaskPage onAdd={addTask} />} 
+          />
+          <Route 
+            path="tasks/:id" 
+            element={<TaskDetailPage tasks={tasks} onUpdate={updateTask} onDelete={deleteTask} />} 
+          />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
-
-export default App
