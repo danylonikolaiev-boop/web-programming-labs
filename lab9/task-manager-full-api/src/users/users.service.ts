7f8c6ddaabd0525346ea47dbd37196e1a8bc80ca
Ніколаєв <danylo.nikolaiev@kgemt.org.ua>
2026-05-13 01:21:30 +0300

    Lab 9: create users.entity, writing users.service

--- lab9/task-manager-full-api/src/users/users.service.ts
+++ lab9/task-manager-full-api/src/users/users.service.ts
@@ -1,4 +1,33 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, ConflictException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import * as bcrypt from 'bcrypt';
+import { User } from './user.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private usersRepository: Repository<User>,
+  ) {}
+
+  async findByEmail(email: string): Promise<User | null> {
+    return this.usersRepository.findOne({ where: { email } });
+  }
+
+  async create(registerDto: RegisterDto): Promise<User> {
+    const existingUser = await this.findByEmail(registerDto.email);
+    if (existingUser) {
+      throw new ConflictException('Користувач з таким email вже існує');
+    }
+
+    const hashedPassword = await bcrypt.hash(registerDto.password, 10);
+    
+    const newUser = this.usersRepository.create({
+      email: registerDto.email,
+      password: hashedPassword,
+    });
+
+    return this.usersRepository.save(newUser);
+  }
+}
