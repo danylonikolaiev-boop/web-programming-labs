a4dd27ddbeec83ddebf36845f7ec80c2b8dcc5c6
Ніколаєв <danylo.nikolaiev@kgemt.org.ua>
2026-05-13 19:51:38 +0300

    Lab 9: creating jwt-auth.guard

--- lab9/task-manager-full-api/src/auth/auth.module.ts
+++ lab9/task-manager-full-api/src/auth/auth.module.ts
@@ -2,11 +2,10 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UsersModule } from '../users/users.module';
-import { JwtModule } from '@nestjs/jwt';
+import { JwtModule, JwtModuleOptions } from '@nestjs/jwt'; 
 import { PassportModule } from '@nestjs/passport';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { JwtStrategy } from './strategies/jwt.strategy';
-
 
 @Module({
   imports: [
@@ -15,9 +14,11 @@ import { JwtStrategy } from './strategies/jwt.strategy';
     JwtModule.registerAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
-      useFactory: async (configService: ConfigService) => ({
+      useFactory: async (configService: ConfigService): Promise<JwtModuleOptions> => ({
         secret: configService.get<string>('JWT_SECRET'),
-        signOptions: { expiresIn: configService.get<string>('JWT_EXPIRES_IN') },
+        signOptions: { 
+          expiresIn: configService.get<string>('JWT_EXPIRES_IN') as any,
+        },
       }),
     }),
   ],
