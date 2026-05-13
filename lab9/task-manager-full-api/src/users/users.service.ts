1e7ff9668e5ab7f42c27d52b6841a701dc8a2454
Ніколаєв <danylo.nikolaiev@kgemt.org.ua>
2026-05-13 18:47:56 +0300

    Lab 9: writing login.dto

--- lab9/task-manager-full-api/src/users/users.service.ts
+++ lab9/task-manager-full-api/src/users/users.service.ts
@@ -3,6 +3,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import * as bcrypt from 'bcrypt';
 import { User } from './user.entity';
+import { RegisterDto } from 'src/auth/dto/register.dto';
 
 
 @Injectable()
